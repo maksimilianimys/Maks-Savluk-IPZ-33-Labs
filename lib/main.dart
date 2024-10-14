4ec1ec2cde7b35bda1096fb6851252e9c5df9295
maksimilianimys <maswety8@example.com>
2024-10-14 23:01:11 +0300

    Lab-6

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,22 @@
 import 'package:flutter/material.dart';
+import './widgets/sign_in_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-const _size = 25.0;
-const _bgColor = Colors.green;
-const _borderColor = Colors.grey;
-const _borderWidth = 3.0;
-const _borderRadius = 4.0;
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lab-5',
+      title: 'Lab-6',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: false,
       ),
-      home: const MyHomePage(title: 'Max Lab-5 Ipz-33'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: _buildFour(),
-      ),
-    );
-  }
-
-  List<Widget> _buildFour() {
-    return [
-      _buildLineTopBottom(),
-      _buildLineTopBottom(),
-      _buildLine(),
-      _buildLineSide(),
-      _buildLineSide(),
-      _buildLineSide(),
-    ];
-  }
-
-  Widget _buildLineTopBottom() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        _buildDot(Colors.green),
-        _buildEmpty(),
-        _buildEmpty(),
-        _buildDot(Colors.green),
-      ],
-    );
-  }
-
-  Widget _buildLine() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        _buildDot(Colors.green),
-        _buildDot(Colors.green),
-        _buildDot(Colors.green),
-        _buildDot(Colors.green),
-      ],
-    );
-  }
-
-  Widget _buildLineSide() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        _buildEmpty(),
-        _buildEmpty(),
-        _buildEmpty(),
-        _buildDot(Colors.green),
-      ],
-    );
-  }
-
-  Widget _buildDot(Color color) {
-    return Container(
-      width: _size,
-      height: _size,
-      decoration: BoxDecoration(
-        color: _bgColor,
-        borderRadius: const BorderRadius.all(
-          Radius.circular(_borderRadius),
-        ),
-        border: Border.all(
-          color: _borderColor,
-          width: _borderWidth,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildEmpty() {
-    return const SizedBox(
-      width: _size,
-      height: _size,
+      home: const SignInScreen(),
     );
   }
 }
---
+++ lib/widgets/reset_password_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class ResetPasswordScreen extends StatelessWidget {
+  const ResetPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: Image.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Google-flutter-logo.svg/1024px-Google-flutter-logo.svg.png",
+                  width: 200,
+                ),
+              ),
+
+              /**/
+
+              const SizedBox(height: 20),
+              const Center(
+                child: Text("Password Reset",
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              ),
+              const SizedBox(height: 12),
+
+              /**/
+
+              const Text('Email',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 12),
+              const TextField(
+                decoration: InputDecoration(
+                    labelText: 'Write you email', border: OutlineInputBorder()),
+              ),
+              const SizedBox(height: 18),
+
+              /**/
+
+              Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 48,
+                      child: ElevatedButton(
+                        onPressed: () => {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext ctx) {
+                              return const AlertDialog(
+                                title: Text('Message'),
+                                content: Text("Need to implement"),
+                              );
+                            },
+                          ),
+                        },
+                        child: const Text("Reset Password"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 48,
+                      child: ElevatedButton(
+                        onPressed: () => {
+                          Navigator.pop(context),
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent, // Прозорий фон
+                          foregroundColor: Colors.blue, // Колір тексту
+                          shadowColor: Colors.transparent, // Прибираємо тінь
+                          side: const BorderSide(
+                              color: Colors.blue,
+                              width: 1), // Колір та ширина обводки
+                        ),
+                        child: const Text("Back"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}
