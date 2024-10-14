e38b94200d917890e05388ff8906fa99d3c68b5d
maksimilianimys <maswety8@example.com>
2024-10-14 23:00:06 +0300

    Lab-5

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,25 @@ void main() {
   runApp(const MyApp());
 }
 
+const _size = 25.0;
+const _bgColor = Colors.green;
+const _borderColor = Colors.grey;
+const _borderWidth = 3.0;
+const _borderRadius = 4.0;
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lab-5',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Ipz-33: Maks First Flutter App'),
+      home: const MyHomePage(title: 'Max Lab-5 Ipz-33'),
     );
   }
 }
@@ -30,14 +37,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter+=4;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,25 +44,81 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: _buildFour(),
+      ),
+    );
+  }
+
+  List<Widget> _buildFour() {
+    return [
+      _buildLineTopBottom(),
+      _buildLineTopBottom(),
+      _buildLine(),
+      _buildLineSide(),
+      _buildLineSide(),
+      _buildLineSide(),
+    ];
+  }
+
+  Widget _buildLineTopBottom() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildDot(Colors.green),
+        _buildEmpty(),
+        _buildEmpty(),
+        _buildDot(Colors.green),
+      ],
+    );
+  }
+
+  Widget _buildLine() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildDot(Colors.green),
+        _buildDot(Colors.green),
+        _buildDot(Colors.green),
+        _buildDot(Colors.green),
+      ],
+    );
+  }
+
+  Widget _buildLineSide() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildEmpty(),
+        _buildEmpty(),
+        _buildEmpty(),
+        _buildDot(Colors.green),
+      ],
+    );
+  }
+
+  Widget _buildDot(Color color) {
+    return Container(
+      width: _size,
+      height: _size,
+      decoration: BoxDecoration(
+        color: _bgColor,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(_borderRadius),
+        ),
+        border: Border.all(
+          color: _borderColor,
+          width: _borderWidth,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.camera_alt_outlined),
-      ),
+    );
+  }
+
+  Widget _buildEmpty() {
+    return const SizedBox(
+      width: _size,
+      height: _size,
     );
   }
 }
